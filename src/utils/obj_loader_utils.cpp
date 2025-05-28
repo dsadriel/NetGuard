85a7766f8cffb58a86da89eba68b4b0ae2ee8233
Adriel de Souza <ds.adriel@gmail.com>
2025-05-28 20:55:34 -0300

    feat: implement triangle building and normal computation for ObjModel

---
+++ src/utils/obj_loader_utils.cpp
@@ -0,0 +1,191 @@
+
+// clang-format off
+
+#include <glad/glad.h>   // Criação de contexto OpenGL 3.3
+#include <GLFW/glfw3.h>  // Criação de janelas do sistema operacional
+#include <glm/vec4.hpp>
+#include <cassert>
+
+#include "utils/obj_loader_utils.hpp"
+#include "core/scene.hpp"
+#include "matrices.hpp"
+
+// clang-format on
+
+// Constrói triângulos para futura renderização a partir de um ObjModel.
+void BuildTrianglesAndAddToVirtualScene(ObjModel *model, map<string, SceneObject> *virtualScene) {
+	GLuint vertex_array_object_id;
+	glGenVertexArrays(1, &vertex_array_object_id);
+	glBindVertexArray(vertex_array_object_id);
+
+	std::vector<GLuint> indices;
+	std::vector<float> model_coefficients;
+	std::vector<float> normal_coefficients;
+	std::vector<float> texture_coefficients;
+
+	for (size_t shape = 0; shape < model->shapes.size(); ++shape) {
+		size_t first_index = indices.size();
+		size_t num_triangles = model->shapes[shape].mesh.num_face_vertices.size();
+
+		for (size_t triangle = 0; triangle < num_triangles; ++triangle) {
+			assert(model->shapes[shape].mesh.num_face_vertices[triangle] == 3);
+
+			for (size_t vertex = 0; vertex < 3; ++vertex) {
+				tinyobj::index_t idx = model->shapes[shape].mesh.indices[3 * triangle + vertex];
+
+				indices.push_back(first_index + 3 * triangle + vertex);
+
+				const float vx = model->attrib.vertices[3 * idx.vertex_index + 0];
+				const float vy = model->attrib.vertices[3 * idx.vertex_index + 1];
+				const float vz = model->attrib.vertices[3 * idx.vertex_index + 2];
+				// printf("tri %d vert %d = (%.2f, %.2f, %.2f)\n", (int)triangle, (int)vertex, vx, vy, vz);
+				model_coefficients.push_back(vx);   // X
+				model_coefficients.push_back(vy);   // Y
+				model_coefficients.push_back(vz);   // Z
+				model_coefficients.push_back(1.0f); // W
+
+				// Inspecionando o código da tinyobjloader, o aluno Bernardo
+				// Sulzbach (2017/1) apontou que a maneira correta de testar se
+				// existem normais e coordenadas de textura no ObjModel é
+				// comparando se o índice retornado é -1. Fazemos isso abaixo.
+
+				if (idx.normal_index != -1) {
+					const float nx = model->attrib.normals[3 * idx.normal_index + 0];
+					const float ny = model->attrib.normals[3 * idx.normal_index + 1];
+					const float nz = model->attrib.normals[3 * idx.normal_index + 2];
+					normal_coefficients.push_back(nx);   // X
+					normal_coefficients.push_back(ny);   // Y
+					normal_coefficients.push_back(nz);   // Z
+					normal_coefficients.push_back(0.0f); // W
+				}
+
+				if (idx.texcoord_index != -1) {
+					const float u = model->attrib.texcoords[2 * idx.texcoord_index + 0];
+					const float v = model->attrib.texcoords[2 * idx.texcoord_index + 1];
+					texture_coefficients.push_back(u);
+					texture_coefficients.push_back(v);
+				}
+			}
+		}
+
+		size_t last_index = indices.size() - 1;
+
+		SceneObject theobject;
+		theobject.name = model->shapes[shape].name;
+		theobject.first_index = first_index;                  // Primeiro índice
+		theobject.num_indices = last_index - first_index + 1; // Número de indices
+		theobject.rendering_mode = GL_TRIANGLES; // Índices correspondem ao tipo de rasterização GL_TRIANGLES.
+		theobject.vertex_array_object_id = vertex_array_object_id;
+
+		(*virtualScene)[model->shapes[shape].name] = theobject;
+	}
+
+	GLuint VBO_model_coefficients_id;
+	glGenBuffers(1, &VBO_model_coefficients_id);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_model_coefficients_id);
+	glBufferData(GL_ARRAY_BUFFER, model_coefficients.size() * sizeof(float), NULL, GL_STATIC_DRAW);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, model_coefficients.size() * sizeof(float), model_coefficients.data());
+	GLuint location = 0;            // "(location = 0)" em "shader_vertex.glsl"
+	GLint number_of_dimensions = 4; // vec4 em "shader_vertex.glsl"
+	glVertexAttribPointer(location, number_of_dimensions, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(location);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	if (!normal_coefficients.empty()) {
+		GLuint VBO_normal_coefficients_id;
+		glGenBuffers(1, &VBO_normal_coefficients_id);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO_normal_coefficients_id);
+		glBufferData(GL_ARRAY_BUFFER, normal_coefficients.size() * sizeof(float), NULL, GL_STATIC_DRAW);
+		glBufferSubData(GL_ARRAY_BUFFER, 0, normal_coefficients.size() * sizeof(float), normal_coefficients.data());
+		location = 1;             // "(location = 1)" em "shader_vertex.glsl"
+		number_of_dimensions = 4; // vec4 em "shader_vertex.glsl"
+		glVertexAttribPointer(location, number_of_dimensions, GL_FLOAT, GL_FALSE, 0, 0);
+		glEnableVertexAttribArray(location);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+	}
+
+	if (!texture_coefficients.empty()) {
+		GLuint VBO_texture_coefficients_id;
+		glGenBuffers(1, &VBO_texture_coefficients_id);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO_texture_coefficients_id);
+		glBufferData(GL_ARRAY_BUFFER, texture_coefficients.size() * sizeof(float), NULL, GL_STATIC_DRAW);
+		glBufferSubData(GL_ARRAY_BUFFER, 0, texture_coefficients.size() * sizeof(float), texture_coefficients.data());
+		location = 2;             // "(location = 1)" em "shader_vertex.glsl"
+		number_of_dimensions = 2; // vec2 em "shader_vertex.glsl"
+		glVertexAttribPointer(location, number_of_dimensions, GL_FLOAT, GL_FALSE, 0, 0);
+		glEnableVertexAttribArray(location);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+	}
+
+	GLuint indices_id;
+	glGenBuffers(1, &indices_id);
+
+	// "Ligamos" o buffer. Note que o tipo agora é GL_ELEMENT_ARRAY_BUFFER.
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indices_id);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLuint), NULL, GL_STATIC_DRAW);
+	glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, 0, indices.size() * sizeof(GLuint), indices.data());
+	//
+
+	// "Desligamos" o VAO, evitando assim que operações posteriores venham a
+	// alterar o mesmo. Isso evita bugs.
+	glBindVertexArray(0);
+}
+
+// Função que computa as normais de um ObjModel, caso elas não tenham sido
+// especificadas dentro do arquivo ".obj"
+void ComputeNormals(ObjModel *model) {
+	if (!model->attrib.normals.empty())
+		return;
+
+	// Primeiro computamos as normais para todos os TRIÂNGULOS.
+	// Segundo, computamos as normais dos VÉRTICES através do método proposto
+	// por Gouraud, onde a normal de cada vértice vai ser a média das normais de
+	// todas as faces que compartilham este vértice.
+
+	size_t num_vertices = model->attrib.vertices.size() / 3;
+
+	std::vector<int> num_triangles_per_vertex(num_vertices, 0);
+	std::vector<glm::vec4> vertex_normals(num_vertices, glm::vec4(0.0f, 0.0f, 0.0f, 0.0f));
+
+	for (size_t shape = 0; shape < model->shapes.size(); ++shape) {
+		size_t num_triangles = model->shapes[shape].mesh.num_face_vertices.size();
+
+		for (size_t triangle = 0; triangle < num_triangles; ++triangle) {
+			assert(model->shapes[shape].mesh.num_face_vertices[triangle] == 3);
+
+			glm::vec4 vertices[3];
+			for (size_t vertex = 0; vertex < 3; ++vertex) {
+				tinyobj::index_t idx = model->shapes[shape].mesh.indices[3 * triangle + vertex];
+				const float vx = model->attrib.vertices[3 * idx.vertex_index + 0];
+				const float vy = model->attrib.vertices[3 * idx.vertex_index + 1];
+				const float vz = model->attrib.vertices[3 * idx.vertex_index + 2];
+				vertices[vertex] = glm::vec4(vx, vy, vz, 1.0);
+			}
+
+			const glm::vec4 a = vertices[0];
+			const glm::vec4 b = vertices[1];
+			const glm::vec4 c = vertices[2];
+
+			// Cálculo da normal de um triângulo cujos vértices
+			// estão nos pontos "a", "b", e "c", definidos no sentido anti-horário.
+			const glm::vec4 n = glm::vec4(glm::normalize(glm::cross(glm::vec3(b - a), glm::vec3(c - a))), 0.0f);
+
+			for (size_t vertex = 0; vertex < 3; ++vertex) {
+				tinyobj::index_t idx = model->shapes[shape].mesh.indices[3 * triangle + vertex];
+				num_triangles_per_vertex[idx.vertex_index] += 1;
+				vertex_normals[idx.vertex_index] += n;
+				model->shapes[shape].mesh.indices[3 * triangle + vertex].normal_index = idx.vertex_index;
+			}
+		}
+	}
+
+	model->attrib.normals.resize(3 * num_vertices);
+
+	for (size_t i = 0; i < vertex_normals.size(); ++i) {
+		glm::vec4 n = vertex_normals[i] / (float)num_triangles_per_vertex[i];
+		n /= norm(n);
+		model->attrib.normals[3 * i + 0] = n.x;
+		model->attrib.normals[3 * i + 1] = n.y;
+		model->attrib.normals[3 * i + 2] = n.z;
+	}
+}
