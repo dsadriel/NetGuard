4ccf5323ce4853f1fd3f599ce336e8697cf66f16
Nicolas Chin Lee <147880373+nicolasclee@users.noreply.github.com>
2025-06-16 10:53:52 -0300

    Revert "Adding texture to map"

--- src/core/shader_fragment.glsl
+++ src/core/shader_fragment.glsl
@@ -1,11 +1,18 @@
 #version 330 core
 
-in vec2 fragTexCoord;              // coordenadas vindas do vertex shader
-out vec4 color;                    // cor final do pixel
+// Atributos de fragmentos recebidos como entrada ("in") pelo Fragment Shader.
+// Neste exemplo, este atributo foi gerado pelo rasterizador como a
+// interpolação da cor de cada vértice, definidas em "shader_vertex.glsl" e
+// "main.cpp" (array color_coefficients).
+in vec4 cor_interpolada_pelo_rasterizador;
 
-uniform sampler2D TextureImage;    // textura carregada no C++
+// O valor de saída ("out") de um Fragment Shader é a cor final do fragmento.
+out vec4 color;
 
 void main()
 {
-    color = texture(TextureImage, fragTexCoord);
-}
+    // Definimos a cor final de cada fragmento utilizando a cor interpolada
+    // pelo rasterizador.
+    color = cor_interpolada_pelo_rasterizador;
+} 
+
--- src/utils/model_utils.cpp
+++ src/utils/model_utils.cpp
@@ -1,4 +1,5 @@
 #include "utils/model_utils.hpp"
+
 // Função que desenha um objeto armazenado em g_VirtualScene. Veja definição
 // dos objetos na função BuildTrianglesAndAddToVirtualScene().
 void DrawVirtualObject(const char* object_name)
@@ -8,8 +9,6 @@ void DrawVirtualObject(const char* object_name)
     // comentários detalhados dentro da definição de BuildTrianglesAndAddToVirtualScene().
     glBindVertexArray(g_VirtualScene[object_name].vertex_array_object_id);
 
-    glBindTexture(GL_TEXTURE_2D, mapTexture);
-    
     // Pedimos para a GPU rasterizar os vértices dos eixos XYZ
     // apontados pelo VAO como linhas. Veja a definição de
     // g_VirtualScene[""] dentro da função BuildTrianglesAndAddToVirtualScene(), e veja
@@ -25,35 +24,4 @@ void DrawVirtualObject(const char* object_name)
     // "Desligamos" o VAO, evitando assim que operações posteriores venham a
     // alterar o mesmo. Isso evita bugs.
     glBindVertexArray(0);
-}
-
-GLuint LoadTextureFromFile(const std::string &filename)
-{
-    
-{
-    int width, height, channels;
-    unsigned char* image = stbi_load(filename.c_str(), &width, &height, &channels, 0);
-    if (!image)
-    {
-        std::cout << "Erro ao carregar textura: " << filename << "\r\n" << std::endl;
-        return 0;
-    }
-    
-
-    std::cout << "\r\nSucesso ao carregar textura de: " << filename << "\r\n" << std::endl;
-
-    GLuint texId;
-    glGenTextures(1, &texId);
-    glBindTexture(GL_TEXTURE_2D, texId);
-
-    GLenum format = (channels == 4) ? GL_RGBA : GL_RGB;
-    glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, image);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    stbi_image_free(image);
-    return texId;
-    
-}
 }
