c3ba759c115d0c3f359035476fa68cb1d9d193f7
Adriel de Souza <ds.adriel@gmail.com>
2025-07-02 09:43:39 -0300

    feat: refactor camera class to remove glm namespace and add picking ray functionality

--- include/game/camera.hpp
+++ include/game/camera.hpp
@@ -6,34 +6,37 @@
 #include <external/glm/vec4.hpp>
 
 #include "matrices.hpp"
+#include "collisions.hpp"
+
+using namespace glm;
 
 enum class CameraMode { Free, LookAt, TopDown };
 enum class CameraMovement { Forward, Backward, Left, Right, Up, Down };
 
 class Camera {
   private:
-	glm::vec4 view_vector;
+	vec4 view_vector;
 	float yaw;
 	float pitch;
 
   public:
-	glm::vec4 position;
-	glm::vec4 up_vector;
-	glm::vec4 target = glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);
+	vec4 position;
+	vec4 up_vector;
+	vec4 target = vec4(0.0f, 0.0f, 0.0f, 1.0f);
 	CameraMode mode = CameraMode::Free;
 
 	float nearplane = -0.1f;
 	float farplane = -100.0f;
 	double field_of_view = 45.0;
 
-	Camera(glm::vec4 pos, float yaw_rad, float pitch_rad)
-	    : yaw(yaw_rad), pitch(pitch_rad), position(pos), up_vector(glm::vec4(0.0f, 1.0f, 0.0f, 0.0f)) {
+	Camera(vec4 pos, float yaw_rad, float pitch_rad)
+	    : yaw(yaw_rad), pitch(pitch_rad), position(pos), up_vector(vec4(0.0f, 1.0f, 0.0f, 0.0f)) {
 		updateViewVector();
 	}
 
-	glm::mat4 getViewMatrix() {
-		glm::vec4 view_vector_;
-		glm::vec4 up_vec_;
+	mat4 getViewMatrix() {
+		vec4 view_vector_;
+		vec4 up_vec_;
 
 		switch (mode) {
 		case CameraMode::LookAt:
@@ -42,8 +45,8 @@ class Camera {
 			return Matrix_Camera_View(position, view_vector_, up_vec_);
 
 		case CameraMode::TopDown:
-			view_vector_ = glm::vec4(0.0f, -1.0f, 0.0f, 0.0f);
-			up_vec_ = glm::vec4(0.0f, 0.0f, 1.0f, 0.0f);
+			view_vector_ = vec4(0.0f, -1.0f, 0.0f, 0.0f);
+			up_vec_ = vec4(0.0f, 0.0f, 1.0f, 0.0f);
 			return Matrix_Camera_View(position, view_vector_, up_vec_);
 
 		default: // CameraMode::Free ou outras
@@ -53,17 +56,17 @@ class Camera {
 		}
 	}
 
-	glm::mat4 getProjectionMatrix(double screenRatio) {
-		return Matrix_Perspective(glm::radians(field_of_view), screenRatio, nearplane, farplane);
+	mat4 getProjectionMatrix(double screenRatio) {
+		return Matrix_Perspective(radians(field_of_view), screenRatio, nearplane, farplane);
 	}
 
 	void updateViewVector() {
-		view_vector = normalize(glm::vec4(cos(pitch) * sin(yaw), sin(pitch), cos(pitch) * cos(yaw), 0.0f));
+		view_vector = normalize(vec4(cos(pitch) * sin(yaw), sin(pitch), cos(pitch) * cos(yaw), 0.0f));
 	}
 
 	// MARK: Camera Movement
 	void move(CameraMovement direction, float speed) {
-		glm::vec4 right = normalize(crossproduct(view_vector, up_vector));
+		vec4 right = normalize(crossproduct(view_vector, up_vector));
 		switch (direction) {
 		case CameraMovement::Forward:
 			position += view_vector * speed;
@@ -93,12 +96,33 @@ class Camera {
 
 	void changePitch(float delta_pitch) {
 		pitch += delta_pitch;
-		if (pitch > glm::radians(89.0f))
-			pitch = glm::radians(89.0f);
-		if (pitch < glm::radians(-89.0f))
-			pitch = glm::radians(-89.0f);
+		if (pitch > radians(89.0f))
+			pitch = radians(89.0f);
+		if (pitch < radians(-89.0f))
+			pitch = radians(-89.0f);
 		updateViewVector();
 	}
+
+	Ray getPickingRay(float screen_width, float screen_height, float cursor_pos_x, float cursor_pos_y) {
+		float x_ndc = (2.0f * cursor_pos_x) / screen_width - 1.0f;
+		float y_ndc = 1.0f - (2.0f * cursor_pos_y) / screen_height;
+		float z_ndc = this->nearplane;
+
+		vec4 ray_ndc(x_ndc, y_ndc, z_ndc, 1.0f);
+
+		// Convert NDC to camera coordinates
+		mat4 projection_matrix = getProjectionMatrix(screen_width / screen_height);
+		vec4 ray_camera = inverse(projection_matrix) * ray_ndc;
+		vec4 ray_dir_camera = vec4(ray_camera.x, ray_camera.y, -1.0f, 0.0f);
+
+		// Convert camera coordinates to world coordinates
+		mat4 view_matrix = getViewMatrix();
+		vec4 ray_dir_world = inverse(view_matrix) * ray_dir_camera;
+		ray_dir_world = normalize(ray_dir_world);
+
+		return Ray(this->position, ray_dir_world);
+	}
+	
 };
 
 #endif // CAMERA_H
