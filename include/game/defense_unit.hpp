94be6606095e91ee882d174d4b66769ce7adbab9
Adriel de Souza <ds.adriel@gmail.com>
2025-07-06 13:14:58 -0300

    refactor: Simplify DefenseUnit and InvasionUnit class structure
    - Both units now a specializations of GameUnit

--- include/game/defense_unit.hpp
+++ include/game/defense_unit.hpp
@@ -1,42 +1,23 @@
 #ifndef _DEFENSE_UNIT_HPP
 #define _DEFENSE_UNIT_HPP
 
-#include <external/glm/vec4.hpp>
-#include "scene.hpp"
+#include "game_unit.hpp"
 
-using namespace glm;
 
-class GameUnit {
-	private:
-	public:
-		SceneObject* sceneObject;
-		vec4 position; // Position of the unit
-		vec4 rotation; // Rotation of the unit
-		int targetIndex;
-		vec2 target;
-
-	GameUnit() : position(vec4(0.0f)), rotation(vec4(0.0f, 0.0f, 0.0f, 0.0f)), sceneObject(nullptr) {}
-	GameUnit(vec4 pos, SceneObject* obj) : position(pos), rotation(vec4(0.0f, 0.0f, 0.0f, 0.0f)), sceneObject(obj) {}
-	GameUnit(vec4 pos, vec4 rot) : position(pos), rotation(rot), sceneObject(nullptr) {}
-	GameUnit(SceneObject* obj) : position(vec4(INFINITY)), rotation(vec4(0.0f, 0.0f, 0.0f, 0.0f)), sceneObject(obj) {}
-
-	virtual void defenseAction() {}
-	virtual void attackAction() {}
-
-	virtual void draw(GLint model_uniform, GLint object_style_uniform, GLint object_color_uniform, GLint shading_mode_uniform ) {
-		if (sceneObject) {
-			sceneObject->position = position;
-			sceneObject->rotation = rotation;
-			sceneObject->drawObject(model_uniform, object_style_uniform, object_color_uniform, shading_mode_uniform);
-		}
-	}
-};
-
-class AntiVirusUnit : public GameUnit {
+class DefenseUnit : public GameUnit {
 	public:
 	
-	AntiVirusUnit() : GameUnit() {}
-	AntiVirusUnit(SceneObject* obj) : GameUnit(obj) {}
+	DefenseUnit() : GameUnit() {}
+	DefenseUnit(SceneObject* obj) : GameUnit(obj) {}
+
+	virtual void defenseAction() {}
+};
+
+class AntiVirusUnit : public DefenseUnit {
+	public:
+	
+	AntiVirusUnit() : DefenseUnit() {}
+	AntiVirusUnit(SceneObject* obj) : DefenseUnit(obj) {}
 
 	void defenseAction() override {
 		// Implement specific action for AntiVirusUnit
--- include/game/invasion_unit.hpp
+++ include/game/invasion_unit.hpp
@@ -1,24 +1,14 @@
 #ifndef _INVASION_UNIT_HPP
 #define _INVASION_UNIT_HPP
 
-#include <external/glm/vec4.hpp>
+#include "game_unit.hpp"
 
-class InvasionUnit {
-    private:
-      glm::vec4 position;  // Position of the invasion unit
-      glm::vec4 direction; // Direction the unit is facing
-    public:
-      InvasionUnit(glm::vec4 pos, glm::vec4 dir) : position(pos), direction(dir) {};
-  
-      // Getters for position and direction
-      glm::vec4 getPosition() const { return position; }
-      glm::vec4 getDirection() const { return direction; }
-  
-      // Setters for position and direction
-      void setPosition(const glm::vec4 &pos) { position = pos; }
-      void setDirection(const glm::vec4 &dir) { direction = dir; }
-  
-      virtual void invasionAction() {}
-  };
+class InvasionUnint : public GameUnit {
+  public:
+	InvasionUnint() : GameUnit() {}
+	InvasionUnint(SceneObject *obj) : GameUnit(obj) {}
 
-  #endif
+	virtual void attackAction() {}
+};
+
+#endif
